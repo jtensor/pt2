fafa2787d3abcf9ed05f661f0613f3e979cf852f
Jamie Titensor <jamietitensor@gmail.com>
2014-09-24 19:23:49 -0600

    cleaning

--- js/Controllers/drCtrl2.js
+++ js/Controllers/drCtrl2.js
@@ -5,13 +5,18 @@ angular
 		$scope.patient = patientRef.$asObject();
 		$scope.appts = apptsRef.$asArray();
 		$scope.assignments = assignmentsRef.$asArray();
+        $scope.lowerExercises = lowerBodyRef.$asObject();
+        $scope.upperExercises = upperBodyRef.$asObject();
+        $scope.cardio = cardioRef.$asObject();
 
         $scope.days =['Mon', 'Tues', 'Wed', 'Thur', 'Fri'];
 
 		$scope.addAppt = function(newAppt){
 			$scope.appts.$remove(0);
       		$scope.appts.$add(newAppt); 
+            $scope.showing=true;
     	};
+
     	$scope.addAssignment = function(newAssignment){
     		if (typeof newAssignment === 'string'){
     			newAssignment = JSON.parse(newAssignment);
@@ -19,40 +24,44 @@ angular
     		$scope.assignments.$add(newAssignment);
     	};
 
-    	$scope.lowerExercises = lowerBodyRef.$asObject();
-    	$scope.upperExercises = upperBodyRef.$asObject();
-    	$scope.cardio = cardioRef.$asObject();
-
     	$scope.go = function(state) {
   			$state.go(state);
 		};
 
-        var assignments = $scope.assignments;
-        $scope.removeAssignments = function(assignments){
-            for( i = 0; i<assignments.length; i++){
-                $scope.assignments.$remove(i);
-        }
-        };
-
-        $scope.writeID = function(o){
-              assignments.$remove(o.$id);
-            }
         $scope.redo = function(o){
-              assignments.$remove(o.$id);
+              $scope.assignments.$remove(o);
             }
 
-        $scope.exerciseModel = [];
+// This multi-select menu
+
+        $scope.exerciseModel = {};
         $scope.exerciseText = {buttonDefaultText: 'Select Exercises'};
-        $scope.exerciseData =[ {id: "{{upperExercises.externalRotation}}", label: "External Rotation", type: "upper"},
-                    {id: "{{upperExercises.internalRotation}}", label: "Internal Rotation", type: "upper"},
-                    {id: "{{lowerExercises.anklePump}}", label: "Ankle Pumps", type: "lower"},
-                    {id: "{{lowerExercises.quadSets}}", label: "Quadriceps Setting", type: "lower"}
+        $scope.exerciseData =[ 
+                    {id:1, value: "{{upperExercises.externalRotation}}", label: "External Rotation", type: "upper"},
+                    {id:2, value: "{{upperExercises.internalRotation}}", label: "Internal Rotation", type: "upper"},
+                    {id:3, value: "{{upperExercises.biceps}}", label: "Bicep Curl", type: "upper"},
+                    {id:4, value: "{{upperExercises.T}}", label: "Standing T", type: "upper"},
+                    {id:5, value: "{{upperExercises.W}}", label: "W exercise", type: "upper"},
+                    {id:6, value: "{{upperExercises.forwardPunch}}", label: "Forward Punch", type: "upper"},
+                    {id:7, value: "{{upperExercises.hug}}", label: "Dynamic Hug", type: "upper"},
+                    {id:8, value: "{{upperExercises.row}}", label: "Row", type: "upper"},
+                    {id:9, value: "{{upperExercises.wallBall}}", label: "Ball on the Wall", type: "upper"},
+                    {id:10, value: "{{lowerExercises.anklePump}}", label: "Ankle Pumps", type: "lower"},
+                    {id:11, value: "{{lowerExercises.quadSets}}", label: "Quadriceps Setting", type: "lower"},
+                    {id:12, value: "{{lowerExercises.heelSlides}}", label: "Heel Slides", type: "lower"},
+                    {id:13, value: "{{lowerExercises.hipAbduct}}", label: "Hip Abduction", type: "lower"},
+                    {id:14, value: "{{lowerExercises.legRaise}}", label: "Straight Leg Raises", type: "lower"},
+                    {id:15, value: "{{lowerExercises.longArc}}", label: "Long Arc Quads", type: "lower"},
+                    {id:16, value: "{{lowerExercises.shortArc}}", label: "Short Arc Quads", type: "lower"},
+                    {id:17, value: "{{lowerExercises.luck}}", label: "Good Luck", type: "always"},
                      ];
         $scope.exerciseSettings = { groupByTextProvider: function(groupValue) {
             if (groupValue === 'upper'){
                 return 'Upper Body';
-            } else {
+            } else if (groupValue === 'lower') {
                 return 'Lower Body';
+            }else{
+                return 'Add Last'
             }
         }
         };
--- js/Controllers/loginCtrl.js
+++ js/Controllers/loginCtrl.js
@@ -7,11 +7,13 @@ angular
 
 		$scope.logMeIn = function (){
 
+			var Name2 = $scope.Name1[0].toUpperCase()+$scope.Name1.substring(1);
+
 			for (i = 0; i<patients.length; i++){
 				
 				console.log(patients[i].firstName)
 				
-				if(patients[i].firstName === $scope.Name1){
+				if(patients[i].firstName === Name2){
 					var p = patients[i];
 					$state.go('personal', {patientId:p.$id, apptId:p.appts.$id});
 				}
